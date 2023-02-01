31336194752289b9f973ae756a906268c637d822
Abdul-Aziz3 <abdi80@outlook.com>
2023-02-01 21:56:35 +0100

    legger til 2 random tall

--- random_1.py
+++ random_1.py
@@ -2,9 +2,10 @@ import random
 
 def getRandomNumber():
     random_tall = random.randint(1,200)
+    random_tall2 = random.randint(1,200)
 
-    return random_tall
+    return random_tall, random_tall2
 
 print(getRandomNumber())
-print(getRandomNumber())
+
 
